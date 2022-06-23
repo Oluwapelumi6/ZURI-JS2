70454118129283e35ddf2613063f0b70590be7d9
Oluwapelumi6 <pelumiadereti2001@gmail.com>
2022-06-23 12:50:50 +0100

    initialcommit

---
+++ script.js
@@ -0,0 +1,8 @@
+var sign = "Adereti Oluwapelumi Faith"
+var height = "160cm"
+var country = "Nigeria"
+var newline = "\r\n"
+
+
+alert(("Name: " + sign) + newline + ("Height: " + height) + newline + ("Country: " + country))
+
